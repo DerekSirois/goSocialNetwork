089711a55ac1ca59664157528549ea35b152cde8
DerekSirois <dereksirois@hotmail.ca>
2023-09-18 11:56:17 -0400

    Basic app api

---
+++ app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+)
+
+type App struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func New() (*App, error) {
+	cs := "host=localhost user=dev password=abcde dbname=goSocial sslmode=disable"
+	db, err := gorm.Open(postgres.Open(cs), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return &App{
+		DB:     db,
+		Router: mux.NewRouter(),
+	}, nil
+}
+
+func (a *App) Run() {
+	log.Println("Serving on post 8000")
+	log.Fatalln(http.ListenAndServe(":8000", a.Router))
+}
+
+func (a *App) Routes() {
+	a.Router.HandleFunc("/", index()).Methods("GET")
+}
+
+func index() http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "Welcome to the Go social network")
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	fmt.Println("Welcome to the Go social network")
+
+	app, err := New()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	app.Routes()
+	app.Run()
 }
