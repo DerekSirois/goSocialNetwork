3658fc1b9e46033a25ad98e5f8ebdc72dcec1860
DerekSirois <dereksirois@hotmail.ca>
2023-09-19 11:31:57 -0400

    refactor  error handling in controller

--- app/postController.go
+++ app/postController.go
@@ -13,10 +13,7 @@ func (a *App) GetAllPostHandler() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		posts, err := a.GetAllPost()
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusInternalServerError)
+			a.handleError(writer, request, err, http.StatusInternalServerError)
 			return
 		}
 		a.respond(writer, request, posts, http.StatusOK)
@@ -28,18 +25,12 @@ func (a *App) GetPostByIdHandler() http.HandlerFunc {
 		vars := mux.Vars(request)
 		id, err := strconv.ParseUint(vars["id"], 10, 32)
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusBadRequest)
+			a.handleError(writer, request, err, http.StatusBadRequest)
 			return
 		}
 		p, err := a.GetPostById(uint(id))
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusInternalServerError)
+			a.handleError(writer, request, err, http.StatusInternalServerError)
 			return
 		}
 		a.respond(writer, request, p, http.StatusOK)
@@ -51,18 +42,12 @@ func (a *App) CreatePostHandler() http.HandlerFunc {
 		p := &models.Post{}
 		err := json.NewDecoder(request.Body).Decode(p)
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusBadRequest)
+			a.handleError(writer, request, err, http.StatusBadRequest)
 			return
 		}
 		err = a.CreatePost(p)
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusInternalServerError)
+			a.handleError(writer, request, err, http.StatusInternalServerError)
 			return
 		}
 		a.respond(writer, request, &Response{Msg: "Post created successfully"}, http.StatusOK)
@@ -74,27 +59,18 @@ func (a *App) UpdatePostHandler() http.HandlerFunc {
 		p := &models.Post{}
 		err := json.NewDecoder(request.Body).Decode(p)
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusBadRequest)
+			a.handleError(writer, request, err, http.StatusBadRequest)
 			return
 		}
 		vars := mux.Vars(request)
 		id, err := strconv.ParseUint(vars["id"], 10, 32)
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusBadRequest)
+			a.handleError(writer, request, err, http.StatusBadRequest)
 			return
 		}
 		err = a.UpdatePost(*p, uint(id))
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusInternalServerError)
+			a.handleError(writer, request, err, http.StatusInternalServerError)
 			return
 		}
 		a.respond(writer, request, &Response{Msg: "Post updated successfully"}, http.StatusOK)
@@ -106,20 +82,21 @@ func (a *App) DeletePostHandler() http.HandlerFunc {
 		vars := mux.Vars(request)
 		id, err := strconv.ParseUint(vars["id"], 10, 32)
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusBadRequest)
+			a.handleError(writer, request, err, http.StatusBadRequest)
 			return
 		}
 		err = a.DeletePost(uint(id))
 		if err != nil {
-			log.Println(err)
-			a.respond(writer, request, &Response{
-				Msg: err.Error(),
-			}, http.StatusInternalServerError)
+			a.handleError(writer, request, err, http.StatusInternalServerError)
 			return
 		}
 		a.respond(writer, request, &Response{Msg: "Post deleted successfully"}, http.StatusOK)
 	}
+}
+
+func (a *App) handleError(w http.ResponseWriter, r *http.Request, err error, statusCode int) {
+	log.Println(err)
+	a.respond(w, r, &Response{
+		Msg: err.Error(),
+	}, statusCode)
 }
